47186f748728fe60e9f3fa23bd9af33db20a8a4f
Pouya-Kan <pouyakananian@gmail.com>
2019-09-02 22:35:53 +0800

    model performance analysis on clustered data

--- deep_model/main_classifier_complex_cnn.py
+++ deep_model/main_classifier_complex_cnn.py
@@ -48,7 +48,8 @@ tf.flags.DEFINE_float('dropout_prob', .9, 'drop out keep probability')
 # learning parameters
 tf.flags.DEFINE_float('learning_rate', .001, 'learning rate')
 tf.flags.DEFINE_string('activation_func', 'relu', 'activation function')
-tf.flags.DEFINE_integer('num_epochs', 150, 'number of training epochs')
+# tf.flags.DEFINE_integer('num_epochs', 150, 'number of training epochs')
+tf.flags.DEFINE_integer('num_epochs', 100, 'number of training epochs')
 tf.flags.DEFINE_integer('batch_size', 64, 'batch size')
 
 # logging parameters
